4742927d79bbdec5ffcb31efa4563ab02c9b0587
CeciAmpudia <ceciliag.ampudia@gmail.com>
2016-05-08 19:04:28 -0500

    Ejercicios Practicos

---
+++ main.js
@@ -0,0 +1,43 @@
+// #1 PROMEDIO DE PERSONAS
+/*var np = prompt("Numero de personas: ")
+i = 1
+s = 0
+
+while(i <= np) {
+	var estatura = prompt("Estatura: ")
+	s += parseFloat(estatura);
+	i++
+}
+var pe = s/parseInt(np);
+document.write("El promedio de estatura es: " + pe)*/
+
+
+// #2
+
+// #3 Sucesion de Fibonacci
+/*var n = prompt("No. de elementos: ")
+i = 0
+f = 1
+a = 0
+b = 0
+while(i <= n){
+	a = b;
+	b = f;
+	f = a + b;
+	i++
+	document.write("<br>" + f)
+}*/
+
+
+// #4
+
+
+// #5 AHORRO
+a = 0
+
+for(var i = 1: i <= 12; i++){
+	var deposito = prompt("Cantidad depositada: ")
+	a = a + deposito; 
+
+	document.write(a)
+}
